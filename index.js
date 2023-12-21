df5d3285c64717fa001f85ac647d91d7ad0fa5fa
Andrés Sanabria <108693080+ansanabria@users.noreply.github.com>
2023-12-21 12:02:55 -0500

    Initial version of the game Rock, Paper, Scissors

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-const OPTIONS = ["Rock", "Paper", "Scissors"];
+const WINNER_INFO = {
+  scissors: "paper",
+  paper: "rock",
+  rock: "scissors",
+};
+
+const OPTIONS = Object.keys(WINNER_INFO);
 
 function capitalize(selection) {
   return selection[0].toUpperCase() + selection.slice(1).toLowerCase().trim();
@@ -10,40 +16,101 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-  playerSelection = capitalize(playerSelection);
-  if (
-    (playerSelection === "Scissors" && computerSelection === "Rock") ||
-    (playerSelection === "Paper" && computerSelection === "Scissors") ||
-    (playerSelection === "Rock" && computerSelection === "Paper")
-  ) {
-    console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
-    return "computer";
-  } else if (playerSelection === computerSelection) {
-    console.log("It's a tie!");
-  } else {
-    console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
+  if (playerSelection === computerSelection) {
+    return "tie";
+  } else if (computerSelection === WINNER_INFO[playerSelection]) {
     return "player";
+  } else {
+    return "computer";
   }
 }
 
 function game() {
+  const dialog = document.querySelector(".dialog");
+  const winnerTag = document.querySelector(".winner-tag");
+  const dialogButton = document.querySelector(".dialog button");
+  const playerButtons = document.querySelectorAll(".player .card");
   const scores = {
-    player: 0,
-    computer: 0,
+    player: {
+      points: 0,
+      domObj: document.querySelector(".player .score-number"),
+    },
+    computer: {
+      points: 0,
+      domObj: document.querySelector(".computer .score-number"),
+    },
+    rounds: 0,
   };
-  for (let i = 0; i < 5; i++) {
-    const playerSelection = prompt("Write your choice");
-    const computerSelection = getComputerChoice();
-    const winner = playRound(playerSelection, computerSelection);
-    if (winner !== undefined) {
-      scores[winner] += 1;
-    }
-  }
-  if (scores["player"] > scores["computer"]) {
-    return "The winner is the player!";
-  } else if (scores["player"] < scores["computer"]) {
-    return "The winner is the computer!";
-  } else {
-    return "The match is a tie! Play again :)";
-  }
+  dialogButton.addEventListener("click", () => {
+    scores.computer.points = 0;
+    scores.player.points = 0;
+    scores.rounds = 0;
+    scores.computer.domObj = scores.computer.points;
+    scores.player.domObj = scores.player.points;
+    dialog.close();
+  });
+  playerButtons.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      const playerSelection = e.currentTarget.id;
+      const computerSelection = getComputerChoice();
+      const winner = playRound(playerSelection, computerSelection);
+      const winnerHeader = document.querySelector(".banner h2");
+      const winnerParagraph = document.querySelector(".banner p");
+      winnerHeader.textContent =
+        winner !== "tie" ? `${capitalize(winner)} wins!` : "It's a tie!";
+      winnerParagraph.textContent = "Make your next move";
+
+      const borderColors = {
+        player: "",
+        computer: "",
+      };
+
+      if (winner !== "tie") {
+        if (winner === "computer") {
+          borderColors.computer = "green";
+          borderColors.player = "red";
+        } else {
+          borderColors.computer = "red";
+          borderColors.player = "green";
+        }
+      } else {
+        borderColors.player = "gray";
+        borderColors.computer = "gray";
+      }
+
+      const playerButton = e.currentTarget;
+      const computerButton = document.querySelector(
+        `.computer .${computerSelection}`
+      );
+      computerButton.style.opacity = "1";
+      computerButton.style.border = `5px solid ${borderColors.computer}`;
+
+      e.currentTarget.style.border = `5px solid ${borderColors.player}`;
+
+      playerButtons.forEach((playerBtn) => {
+        playerBtn.setAttribute("disabled", "");
+      });
+
+      if (winner !== "tie") {
+        scores[winner]["points"]++;
+        scores[winner]["domObj"].textContent = scores[winner]["points"];
+      }
+      scores.rounds++;
+
+      setTimeout(() => {
+        computerButton.style.opacity = "0.5";
+        computerButton.style.border = "3px solid rgba(0, 0, 0, 0.4)";
+        playerButton.style.border = "3px solid rgba(0, 0, 0, 0.4)";
+        playerButtons.forEach((playerBtn) => {
+          playerBtn.removeAttribute("disabled");
+        });
+        if (scores.player.points === 5 || scores.computer.points === 5) {
+          winnerTag.textContent = capitalize(winner);
+          dialog.showModal();
+        }
+      }, 3000);
+    });
+  });
 }
+
+game();
